d0faa13fb798e95facec28489183023c32250ebe
RishikaYashwini <rishika.yashwini@gmail.com>
2026-03-09 05:18:32 +0530

    Day6: Basic Maths - Divisors of a  Number(with log(sqrt(n)) as Complexity)

--- Basics/BasicMaths/prtDivisor.cpp
+++ Basics/BasicMaths/prtDivisor.cpp
@@ -10,11 +10,15 @@ using namespace std;
 
 vector<int> divisors(int n) {
     vector<int> ans;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i*i <= n; i++) {
         if (n % i == 0) {
             ans.push_back(i);
+            if (i != n / i) {
+                ans.push_back(n / i);
+            }
         }
     }
+    sort(ans.begin(), ans.end());
     return ans;
 }
 
@@ -24,8 +28,8 @@ int main() {
     cin >> n;
     vector<int> result = divisors(n);
     cout << "Divisors of " << n << " are: ";
-    for (int i = 0; i < result.size(); i++) {
-        cout << result[i] << " ";
+    for (auto it : result) {
+        cout << it << " ";
     }
     cout << endl;
     return 0;
