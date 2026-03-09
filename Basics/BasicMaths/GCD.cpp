b9d868024e79d05e43c7d7e1ca0597f6e9a96903
RishikaYashwini <rishika.yashwini@gmail.com>
2026-03-09 05:53:20 +0530

    Day6: Basic Maths - GCD of two numbers

--- Basics/BasicMaths/GCD.cpp
+++ Basics/BasicMaths/GCD.cpp
@@ -7,3 +7,17 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
+int gcd(int n1, int n2) {
+    if (n2 == 0) return n1; // Base case: if n2 is 0, GCD is n1
+    return gcd(n2, n1 % n2); // Recursive call with n2 and the remainder of n1 divided by n2
+}
+
+int main() {
+    int n1, n2;
+    cout << "Enter two numbers: ";
+    cin >> n1 >> n2;
+    int result = gcd(n1, n2);
+    cout << "GCD of " << n1 << " and " << n2 << " is: " << result << endl;
+    return 0;
+}
