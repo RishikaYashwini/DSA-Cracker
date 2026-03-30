db2c13f7fc84fc9884bacd9841d0e04d37fbd130
RishikaYashwini <rishika.yashwini@gmail.com>
2026-03-30 12:11:07 +0530

    Day 9: Sorting Techniques - BubbleSort using vector container

--- SortingTechniques/BubbleSort.cpp
+++ SortingTechniques/BubbleSort.cpp
@@ -12,31 +12,43 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(int arr[], int n) {
-    for (int i = n-1; i >= 1; i--) {
-        for (int j = 0; j <= i-1; j++) {
-            if(arr[j] > arr[j+1]) {
-                swap(arr[j], arr[j+1]);
+// void bubbleSort(int arr[], int n) {
+//     for (int i = n-1; i >= 1; i--) {
+//         for (int j = 0; j <= i-1; j++) {
+//             if(arr[j] > arr[j+1]) {
+//                 swap(arr[j], arr[j+1]);
+//             }
+//         }
+//     }
+// }
+
+vector<int> BubbleSort(vector<int>& nums) {
+    int n = nums.size();
+    for(int i=n-1; i>=1; i--) {
+        for(int j=0; j<=i-1; j++) {
+            if(nums[j] > nums[j+1]) {
+                swap(nums[j], nums[j+1]);
             }
         }
     }
+    return nums;
 }
 
 int main() {
     int n;
     cout << "Enter the number of elements in the array: ";
     cin >> n;
-    int arr[n];
+    vector<int> nums(n);
     cout << "Enter the elements of the array: ";
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> nums[i];
     }
     
-    bubbleSort(arr, n);
+    nums = BubbleSort(nums);
     
     cout << "Sorted array: ";
     for (int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
+        cout << nums[i] << " ";
     }
     
     return 0;
